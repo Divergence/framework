4b86467551b4103bf222c0b949196aa8da3bc00f
Henry Paradiz <digitalisakujin@gmail.com>
2018-04-29 19:26:08 -0400

    Changed initial db.php config file

--- config/db.php
+++ config/db.php
@@ -7,11 +7,11 @@ return [
      *
      */
     'mysql' => [
-        'host'      =>  'localhost'
-        ,'database' =>  'divergence'
-        ,'username' =>  'divergence'
-        ,'password' =>  'abc123',
-    ]
+        'host'      =>  'localhost',
+        'database' =>  'divergence',
+        'username' =>  'divergence',
+        'password' =>  'abc123',
+    ],
     /*
      *	This configuration will be used instead of the default if
      *	the file .dev is detected inside of $_SERVER['DOCUMENT_ROOT']
@@ -19,10 +19,21 @@ return [
      *	You may override this behaviour in /config/DB.config.php
      *
      */
-    ,'dev-mysql' => [
-        'host'      =>  'localhost'
-        ,'database' =>  'divergence'
-        ,'username' =>  'divergence'
-        ,'password' =>  'abc123',
+    'dev-mysql' => [
+        'host'      =>  'localhost',
+        'database' =>  'divergence',
+        'username' =>  'divergence',
+        'password' =>  'abc123',
     ],
+    /*
+     *	If you plan on working on the framework itself it's a good idea to provide
+     *	an empty database for unit testing.
+     *
+     */
+    'tests-mysql' => [
+        'host'     =>   'localhost',
+        'database' =>  'divergence_unit_tests',
+        'username' =>  'divergence_unit_tests',
+        'password' =>  'abc123',
+    ]
 ];
