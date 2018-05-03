86031f90888a46094e3967a26e570b637097f5e7
Henry Paradiz <digitalisakujin@gmail.com>
2018-05-03 14:05:27 -0400

    SQL::getCreateTable Test

--- tests/Divergence/IO/Database/SQLTest.php
+++ tests/Divergence/IO/Database/SQLTest.php
@@ -8,6 +8,7 @@ use Divergence\IO\Database\MySQL as DB;
 use Divergence\IO\Database\SQL;
 
 use Divergence\Tests\MockSite\Models\Tag;
+use Divergence\Tests\MockSite\Models\Canary;
 
 class SQLTest extends TestCase
 {
@@ -37,5 +38,23 @@ class SQLTest extends TestCase
         
         $this->assertEquals($safeLittleBobbyTables, SQL::escape($littleBobbyTables));
         $this->assertEquals($safeArrayOfBobbies, SQL::escape($arrayOfBobbies));
+    }
+
+    public function testGetCreateTable()
+    {
+        $Expected[Tag::class] = 'ae3e735ba26bdd70332877d0458a5ff98a6580dc';
+        $Expected[Canary::class] = '6aadc6f7938c72d1bf78c4eb8a20ba2966b773fd';
+
+        foreach($Expected as $Class=>$Hash) {
+            $this->assertEquals($Hash,sha1(SQL::getCreateTable($Class)));
+        }
+    }
+
+    public function testGetCreateTableVersioned()
+    {
+        $Expected[Canary::class] = 'f6f9210d08baae57bad192b7475552071fbc9d82';
+        foreach($Expected as $Class=>$Hash) {
+            $this->assertEquals($Hash,sha1(SQL::getCreateTable($Class,true)));
+        }
     }
 }
