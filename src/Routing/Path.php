3147d141437c92e2666ba4d32dd5a745e5aa488d
Henry Paradiz <henry.paradiz@gmail.com>
2020-06-22 04:20:47 -0400

    Update Path.php

--- src/Routing/Path.php
+++ src/Routing/Path.php
@@ -24,35 +24,44 @@ class Path
         $this->setPath($path);
     }
 
+    /**
+     * If path is /my/example this function returns my the first time and example the second time
+     * False is returned once you run out of path.
+     * @return string|false
+     */
     public function peekPath()
     {
-        if (!isset($this->_path)) {
-            $this->setPath();
-        }
         return count($this->_path) ? $this->_path[0] : false;
     }
 
+    /**
+     * The shifted value, or null; if array is empty or is not an array
+     *
+     * @return mixed
+     */
     public function shiftPath()
     {
-        if (!isset($this->_path)) {
-            $this->setPath();
-        }
         return array_shift($this->_path);
     }
 
+    /**
+     * Returns the path stack
+     *
+     * @return array
+     */
     public function getPath()
     {
-        if (!isset($this->_path)) {
-            $this->setPath();
-        }
         return $this->_path;
     }
 
+    /**
+     * Adds a value to the path stack so that the next shiftPath or peekPath is what you provide here
+     *
+     * @param string $string
+     * @return int Count of path stack after appending to it
+     */
     public function unshiftPath($string)
     {
-        if (!isset($this->_path)) {
-            $this->setPath();
-        }
         return array_unshift($this->_path, $string);
     }
 
